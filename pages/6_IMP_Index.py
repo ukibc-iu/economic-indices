5ce49762d71b3f4e16ae27828474bfdeaab83506
simm2203 <simranguptaa2203@gmail.com>
2025-06-18 10:07:56 +0530

    Update 6_IMP_Index.py

--- pages/6_IMP_Index.py
+++ pages/6_IMP_Index.py
@@ -6,6 +6,8 @@ import plotly.graph_objects as go
 st.set_page_config(layout="wide")
 st.title("IMP Index Dashboard")
 
+st.markdown("India’s Macroeconomic Performance (IMP) Index measures India's overall economic well-being, taking into consideration significant economic parameters such as inflation rate, unemployment rate, etc.")
+
 # === Load IMP Index Data ===
 imp_data_path = "data/IMP_Index.csv"
 try:
